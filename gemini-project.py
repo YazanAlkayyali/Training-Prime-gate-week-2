6cd1fc64bc0367b8dd3cd4095f85d01a9cedeaa6
yazank <yazank105@gmail.com>
2026-09-06 15:56:46 +0300

    import model

--- gemini-project.py
+++ gemini-project.py
@@ -1,6 +1,21 @@
 import os
 from dotenv import load_dotenv
+from groq import Groq
 
 load_dotenv()
-API_KEY = os.getenv("GEMINI_API_KEY")
 
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "name 10 contries on the middle east",
+        }
+    ],
+    model="openai/gpt-oss-120b",
+)
+
+print(chat_completion.choices[0].message.content)
