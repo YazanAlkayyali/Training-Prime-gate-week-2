d8986ace533ae5e1d834b4da6be192202f174eec
yazank <yazank105@gmail.com>
2026-09-07 09:30:32 +0300

    add LangFuse loging feature

--- gemini-projectWlangchain.py
+++ gemini-projectWlangchain.py
@@ -3,9 +3,19 @@ from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.messages import HumanMessage, AIMessage
+from langfuse import Langfuse
+from langfuse.langchain import CallbackHandler
+
 
 load_dotenv()
 API_KEY = os.getenv("GROQ_API_KEY")
+
+Langfuse(
+    public_key=os.getenv("LANGFUSE_PUBLIC_KEY"),
+    secret_key=os.getenv("LANGFUSE_SECRET_KEY"),
+    host=os.getenv("LANGFUSE_HOST"),
+)
+langfuse_handler = CallbackHandler()
 
 model = ChatGroq(
     model="openai/gpt-oss-20b",
@@ -25,11 +35,11 @@ chain = prompt | model
 history = []
 
 def chat(user_input: str) -> str:
-    response = chain.invoke({"history": history, "input": user_input})
+    response = chain.invoke({"history": history, "input": user_input}, config={"callbacks": [langfuse_handler]})
     history.append(HumanMessage(content=user_input))
     history.append(AIMessage(content=response.content))
     return response.content
 
-print(chat("My name is Yazan, i am 19 yearsold."))
-print(chat("What's my name?"))
-print(chat("What's my age?"))
+print(chat("what color is the sky, its my favourit color"))
+print(chat('who were the countries in world war 2'))
+print(chat("What is my favorite color"))
